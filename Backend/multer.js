09bee978d52475205e2a15a9d47d648896c1d029
Jitesh-Gadage16 <gadagejitesh16@gmail.com>
2024-06-19 11:35:45 +0530

    chnages

--- Backend/multer.js
+++ Backend/multer.js
@@ -6,29 +6,29 @@ const multer = require('multer')
 // });
 
 const storage = multer.diskStorage({
-  destination: function (req,file,cb){
+  destination: function (req, file, cb) {
     cb(null, './uploads/')
   },
-  filename: function(req, file, cb){
-    cb(null,new Date.toString() +  '-' +  file.originalname)
+  filename: function (req, file, cb) {
+    cb(null, new Date.toString() + '-' + file.originalname)
   }
 
 
 })
 
-const fileFilter = (req,file,cb) => {
-  if(file.mimetype === 'img/jpeg' || file.mimetype === 'img/png'){
-    cb(null,true)
-  } 
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'img/jpeg' || file.mimetype === 'img/png') {
+    cb(null, true)
+  }
   else {
-    cb({Message :'unsuppoerted filr format'}, false)
+    cb({ Message: 'unsuppoerted filr format' }, false)
   }
 }
 
 const upload = multer({
-  storage:storage,
-  limits:{fileSize:1024*1024},
-  fileFilter:fileFilter
+  storage: storage,
+  limits: { fileSize: 50 * 1024 * 1024 },
+  fileFilter: fileFilter
 })
 
 module.exports = upload;
